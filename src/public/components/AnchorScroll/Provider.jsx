58cb35f009581f59ae4450135e8c5f7fc7d236bd
Dmitry Shuranov <dvshur@gmail.com>
2017-08-03 11:48:16 +0300

    removed setTimeout

--- src/public/components/AnchorScroll/Provider.jsx
+++ src/public/components/AnchorScroll/Provider.jsx
@@ -22,7 +22,7 @@ class Provider extends Component {
         const hash = window.location.hash.substring(1);
         const element = document.querySelector(`[data-anchor='${hash}']`);
         if (element) {
-            setTimeout(() => smoothScrollTo(element, { duration: this.props.duration }), 0);
+            smoothScrollTo(element, { duration: this.props.duration });
         }
     }
 
