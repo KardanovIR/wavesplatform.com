f4d4434694dcb885b8db309cb16c4723c79e0b1a
Dmitry Shuranov <dvshur@gmail.com>
2017-08-07 18:08:10 +0300

    background component

---
+++ src/common/components/Background/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import injectSheet from 'react-jss';
+
+
+
+const styles = {
+    wrapper: {
+        position: 'relative',
+        overflow: 'visible'
+    },
+    background: {
+        position: 'absolute',
+        width: 3000,
+        height: '100%',
+
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%) rotate(-3deg)',
+
+        zIndex: -1
+    }
+}
+
+
+const Background = ({ classes, children, className, style }) => (
+    <div className={classes.wrapper}>
+        <div className={classes.background + ' ' + className} style={style} />
+        { children }
+    </div>
+)
+
+
+Background.defaultProps = {
+    className: ''
+}
+
+
+export default injectSheet(styles)(Background);
--- src/common/containers/Main/index.jsx
+++ src/common/containers/Main/index.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 // components
 import ThemeProvider from '../../components/ThemeProvider';
 import Layout from '../../components/Layout';
-import Typography from '../../components/Typography';
-import { Row, Col } from '../../components/Grid';
-import Button from '../../components/Button';
+// import Typography from '../../components/Typography';
+// import { Row, Col } from '../../components/Grid';
+// import Button from '../../components/Button';
+import Background from '../../components/Background';
 
 
 // containers
@@ -23,6 +24,9 @@ import injectSheet from 'react-jss';
 const PageLayout = ({ classes }) => (
     <Layout>
         <MainScreen />
+        <Background style={{ background: 'lightgreen' }}>
+            <div style={{height: 500, background: 'lightcyan'}}></div>
+        </Background>
     </Layout>
 );
 
