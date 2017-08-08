93084414c91375e14e41170b5014ce98e927c842
Dmitry Shuranov <dvshur@gmail.com>
2017-08-08 14:58:24 +0300

    added centered Row prop, updated Main layout

--- src/common/components/Grid/Row.js
+++ src/common/components/Grid/Row.js
@@ -15,16 +15,38 @@ const styles = theme => ({
         // '& $row': {
         //     margin: theme.spacing.getSpacing(0, -1)
         // }
+    },
+    centered: {
+        justifyContent: 'center'
     }
 });
 
 
 
-const Row = ({ classes, className, children }) => (
-    <div className={cn(classes.row, className)}>
-        {children}
-    </div>
-);
+const Row = ({
+    classes,
+    className: classNameProp,
+    children,
+    centered
+}) => {
+    const className = cn(
+        classes.row,
+        {
+            [classes.centered]: centered
+        },
+        classNameProp
+    );
+
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    )
+}
+
+Row.defaultProps = {
+    centered: false
+}
 
 
 export default injectSheet(styles)(Row);
--- src/common/containers/Main/index.jsx
+++ src/common/containers/Main/index.jsx
@@ -29,8 +29,7 @@ const styles = theme => ({
         padding: theme.spacing.getSpacing(5, 0)
     },
     centered: {
-        textAlign: 'center',
-        justifyContent: 'center'
+        textAlign: 'center'
     },
     marginSmall: {
         marginBottom: theme.spacing.unit
@@ -49,11 +48,15 @@ const PageLayout = ({ classes: c }) => (
         <MainScreen />
         <Description />
         <WavesClient />
-        <Wallet />
-        <DEX />
 
+        <Row centered>
+            <Col xs={12} md={10}>
+                <Wallet />
+                <DEX />
+            </Col>
+        </Row>
 
-        <Row className={cn(c.centered, c.padded)}>
+        <Row centered className={cn(c.centered, c.padded)}>
             <Col xs={12} sm={8} md={6}>
                 <Typography type="display2" className={c.marginSmall}>
                     Issuing your tokens
@@ -69,7 +72,7 @@ const PageLayout = ({ classes: c }) => (
 
         <Feedback
             image={<div style={{ height: '100%', background: 'lightblue' }} />}
-            logo={<div style={{ height: 20, background: 'red' }} />}
+            logo={<div style={{ height: 20, width: 100, background: 'lightred' }} />}
             description="Worldwide coworking spaces for Tech Community"
             website="primalbase.com"
             title="Primalbase crowdsale ends after one day and over 3 000 BTC raised"
@@ -77,20 +80,32 @@ const PageLayout = ({ classes: c }) => (
             signature="Primalbase Team"
             avatar={<div style={{ height: 90, background: 'red' }} />}
         />
-        
+        <br />
+        <br />
+        <Feedback
+            image={<div style={{ height: '100%', background: 'lightgreen' }} />}
+            logo={<div style={{ height: 20, width: 100, background: 'lightgreen' }} />}
+            description="The Crypto-Centric Mobile Gaming Platform and Store"
+            website="mobilego.io"
+            title="MobileGo becomes largest blockchain crowdsale in history!"
+            quote="	There are advances being made in science and technology everyday, and a good example of this is the LCD monitor. LCD monitors have several benefits over the old chunky computer monitors that most users are familiar with. Old computer monitors, take up quite a bit of desktop space, put out a ton of heat, drain a ton of energy, and most often have low picture quality and resolution. Modern advances with the LCD computer monitors have changed"
+            signature="MobileGo Team"
+            avatar={<div style={{ height: 90, background: 'green' }} />}
+        />
+
     </Layout>
-        );
+);
 
 
-const Page =  injectSheet(styles)(PageLayout);
+const Page = injectSheet(styles)(PageLayout);
 // const Page = PageLayout;
 
 
 const App = () => (
     <ThemeProvider>
-            <Page />
-        </ThemeProvider>
-        )
+        <Page />
+    </ThemeProvider>
+)
 
 
 
