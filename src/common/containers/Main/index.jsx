fe5f0e36c02f4016c3995d83c7a7e59279c695e9
Dmitry Shuranov <dvshur@gmail.com>
2017-08-18 17:59:14 +0300

    description

--- src/common/containers/Main/index.jsx
+++ src/common/containers/Main/index.jsx
@@ -36,91 +36,92 @@ const PageLayout = ({
     initialState,
     classes
 }) => (
-    <Layout invertedNavigation>
+        <Layout invertedNavigation>
 
-        <Background className={classes.bgMain}>
-            <div className={classes.paddedBottom}>
-                <MainScreen dexData={initialState.dexData} />
-            </div>
-        </Background>
-
-        <Background className={classes.backgroundColor}>
-            <div className={classes.withBackground}>
-                <Description />
-            </div>
-        </Background>
-
-        <div className={classes.section}>
-            <WavesClient />
-        </div>
-
-
-        <Row centered>
-            <Col xs={12} md={10}>
-                <Background className={classes.backgroundGradient}>
-                    <div className={classes.sectionNarrow}>
-                        <Wallet />
-                    </div>
-                    <div className={classes.sectionNarrow}>
-                        <DEX />
-                    </div>
-                </Background>
-            </Col>
-        </Row>
-
-        <div className={classes.paddedTop}>
-            <IssuingTokens />
-        </div>
-
-        <div className={classes.section}>
-            <Feedback
-                image={<div style={{ height: '100%', background: 'lightblue' }} />}
-                logo={<div style={{ height: 20, width: 100, background: 'red' }} />}
-                description="Worldwide coworking spaces for Tech Community"
-                website="primalbase.com"
-                title="Primalbase crowdsale ends after one day and over 3 000 BTC raised"
-                quote="We are very grateful to all who participated early and provided strong support to this project. If you were unable to secure your token in time, please stay subscribed to our mailing list to keep informed about project developments and expansions. Although no further PBT sale will ever be carried out, related complementary projects will be launched to help increase the value of PBT for existing investors as well as provide upsides for any new participants"
-                signature="Primalbase Team"
-                avatar={<div style={{ height: 90, background: 'red' }} />}
-            />
+            <Background className={classes.bgMain}>
+                    <MainScreen dexData={initialState.dexData} />
+            </Background>
 
             <div className={classes.sectionNarrow}>
-                <Facts />
+                <Description />
             </div>
 
-            <Feedback
-                image={<div style={{ height: '100%', background: 'lightgreen' }} />}
-                logo={<div style={{ height: 20, width: 100, background: 'lightgreen' }} />}
-                description="The Crypto-Centric Mobile Gaming Platform and Store"
-                website="mobilego.io"
-                title="MobileGo becomes largest blockchain crowdsale in history!"
-                quote="	There are advances being made in science and technology everyday, and a good example of this is the LCD monitor. LCD monitors have several benefits over the old chunky computer monitors that most users are familiar with. Old computer monitors, take up quite a bit of desktop space, put out a ton of heat, drain a ton of energy, and most often have low picture quality and resolution. Modern advances with the LCD computer monitors have changed"
-                signature="MobileGo Team"
-                avatar={<div style={{ height: 90, background: 'green' }} />}
-            />
-        </div>
 
-        <Margin bottom={4} />
+            <Background skewed={true} className={classes.backgroundColor}>
+                <div className={classes.section}>
+                    <WavesClient />
+                </div>
+            </Background>
 
-        <Background className={classes.backgroundColor}>
-            <div className={classes.withBackground}>
-                <PlannedFeatures />
+
+
+
+            <Row centered>
+                <Col xs={12} md={10}>
+                    <Background className={classes.backgroundGradient}>
+                        <div className={classes.sectionNarrow}>
+                            <Wallet />
+                        </div>
+                        <div className={classes.sectionNarrow}>
+                            <DEX />
+                        </div>
+                    </Background>
+                </Col>
+            </Row>
+
+            <div className={classes.paddedTop}>
+                <IssuingTokens />
             </div>
-        </Background>
+
+            <div className={classes.section}>
+                <Feedback
+                    image={<div style={{ height: '100%', background: 'lightblue' }} />}
+                    logo={<div style={{ height: 20, width: 100, background: 'red' }} />}
+                    description="Worldwide coworking spaces for Tech Community"
+                    website="primalbase.com"
+                    title="Primalbase crowdsale ends after one day and over 3 000 BTC raised"
+                    quote="We are very grateful to all who participated early and provided strong support to this project. If you were unable to secure your token in time, please stay subscribed to our mailing list to keep informed about project developments and expansions. Although no further PBT sale will ever be carried out, related complementary projects will be launched to help increase the value of PBT for existing investors as well as provide upsides for any new participants"
+                    signature="Primalbase Team"
+                    avatar={<div style={{ height: 90, background: 'red' }} />}
+                />
+
+                <div className={classes.sectionNarrow}>
+                    <Facts />
+                </div>
+
+                <Feedback
+                    image={<div style={{ height: '100%', background: 'lightgreen' }} />}
+                    logo={<div style={{ height: 20, width: 100, background: 'lightgreen' }} />}
+                    description="The Crypto-Centric Mobile Gaming Platform and Store"
+                    website="mobilego.io"
+                    title="MobileGo becomes largest blockchain crowdsale in history!"
+                    quote="	There are advances being made in science and technology everyday, and a good example of this is the LCD monitor. LCD monitors have several benefits over the old chunky computer monitors that most users are familiar with. Old computer monitors, take up quite a bit of desktop space, put out a ton of heat, drain a ton of energy, and most often have low picture quality and resolution. Modern advances with the LCD computer monitors have changed"
+                    signature="MobileGo Team"
+                    avatar={<div style={{ height: 90, background: 'green' }} />}
+                />
+            </div>
+
+            <Margin bottom={4} />
+
+            <Background className={classes.backgroundColor}>
+                <div className={classes.withBackground}>
+                    <PlannedFeatures />
+                </div>
+            </Background>
 
 
-        <div className={classes.section}>
-            <JoinUs />
-        </div>
+            <div className={classes.section}>
+                <JoinUs />
+            </div>
 
-        <div className={classes.movedDown}>
-            <Subscribe />
-            <Background className={classes.footerDivider} skewed={false} />
-        </div>
+            <div className={classes.movedDown}>
+                <Subscribe />
+                <Background className={classes.footerDivider} skewed={false} />
+            </div>
 
 
-    </Layout>
-);
+        </Layout>
+    );
 
 
 const Page = injectSheet(styles)(PageLayout);
