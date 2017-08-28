f7ad1bf2cffa48e55d6ef733b7941297f6df985c
Dmitry Shuranov <dvshur@gmail.com>
2017-08-28 16:52:29 +0300

    implemented index page main screen with a new MainScreen component

--- src/common/containers/Main/lib/MainScreen/index.jsx
+++ src/common/containers/Main/lib/MainScreen/index.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
 
-import TitleTextCTA from 'src/common/components/TitleTextCTA';
+import MainScreen from 'src/common/components/MainScreen';
 import Button from 'src/common/components/Button';
 
-import { Row, Col } from 'src/common/components/Grid';
 
-
-import injectSheet from 'react-jss';
-import styles from './styles';
+import ImageMobile from '!svg-react-loader!./img/table_mob.svg';
+import Image from '!svg-react-loader!./img/image.svg';
 
 
 
-const MainScreen = ({
-    classes,
-}) => (
-        <Row>
-
-            <Col xs={12} md={7} lg={6} className={classes.titleTextCtaCol}>
-                <TitleTextCTA
-                    title="Get started with blockchain"
-                    text="Gain access to the platform solutions to store, trade, manage and issue your digital assets in an easy and secure way."
-                    inverted
-                    buttons={[
-                        <Button withLoader className={classes.ctaButton} secondary key="main_cta_button" href="/download">Get Client</Button>,
-                        <Button withLoader className={classes.secondCtaButton} bordered key="main_cta_button2" href="/download">Get Waves</Button>,
-                    ]}
-                />
-            </Col>
-
-            <Col xs={12} md={5} lg={6} className={classes.imageCol}>
-                <div className={classes.imageWrapper}>
-                    <div className={classes.image} />
-                </div>
-            </Col>
-
-        </Row>
-    );
+const DevelopersMainScreen = () => (
+    <MainScreen
+        title="Get started with blockchain"
+        text="Gain access to the platform solutions to store, trade, manage and issue your digital assets in an easy and secure way."
+        imageDesktop={<Image />}
+        imageMobile={<ImageMobile />}
+        buttons={[
+            <Button withLoader secondary key="main_cta_button" href="/download">Get Client</Button>,
+            <Button withLoader bordered key="main_cta_button2" href="/download">Get Waves</Button>,
+        ]}
+    />
+);
 
 
-export default injectSheet(styles)(MainScreen);
+export default DevelopersMainScreen;
