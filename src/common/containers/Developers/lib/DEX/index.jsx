bec84cbf284a639a8249bac1ced8739639d5cb2b
Dmitry Shuranov <dvshur@gmail.com>
2017-08-29 14:22:31 +0300

    PictureBlock for Developers page responsive template

--- src/common/containers/Developers/lib/DEX/index.jsx
+++ src/common/containers/Developers/lib/DEX/index.jsx
@@ -1,76 +1,34 @@
 import React from 'react';
 
-import Typography from 'src/common/components/Typography';
 import IconButton from 'src/common/components/Button/Icon';
-import Margin from 'src/common/components/Margin';
-import { Row, Col } from 'src/common/components/Grid';
 
-import dexImageSrc from '!svg-url-loader?noquotes!./img/dex.svg';
+import DexImage from '!svg-react-loader!./img/dex.svg';
 
-
-import injectSheet from 'react-jss';
+import PictureBlock from '../PictureBlock';
 
 
-
-const styles = theme => ({
-    image: {
-        maxWidth: '100%'
-    },
-    [theme.breakpoints.up('md')]: {
-        buttonWrapper: {
-            display: 'inline-block',
-            verticalAlign: 'middle'
-        },
-        text: {
-            paddingLeft: theme.spacing.unit * 4,
-            paddingRight: '10%'
-        },
-        mdAlignCenter: {
-            textAlign: 'center'
-        }
-    },
-})
-
-
-const FriendlyAPI = ({ classes }) => (
-    <div>
-        <Row centered>
-            <Col xs={12} md={8} lg={7}>
-                <Typography type="display2" className={classes.mdAlignCenter}>
-                    Decentralized Exchange
-                </Typography>
-                <Typography type="body2" className={classes.mdAlignCenter}>
-                    Our nodes contain an order matcher which is used as decentralized exchange — it enables token trades that don’t require transferring them from the blockchain to a centralized exchange.
-                </Typography>
-                <Margin bottom={4} />
-            </Col>
-        </Row>
-
-
-        <Row direction="row-reverse">
-            <Col xs={12} md={6} lg={6} className={classes.text}>
-                <Typography type="body" margin>
-                    All operations within the DEX are secure and validated by blockchain nodes. Since the DEX is open source and a part of every node, each user can run their own instance of the DEX. 
-                </Typography>
-                <Typography type="body" margin>
-                    The matcher exposes its own REST API for accessing the order book and submitting orders. This allows users to implement any kind of trading UI or trading robots.
-                </Typography>                
-
-                <Margin bottom={2} />
-                    <IconButton
-                        href="https://github.com/wavesplatform/Waves/wiki/Matcher"
-                        target="_blank"
-                        iconName="github"
-                    >
-                        DEX API
+const DEX = () => (
+    <PictureBlock
+        title="Decentralized Exchange"
+        subtitle="Our nodes contain an order matcher which is used as decentralized exchange — it enables token trades that don’t require transferring them from the blockchain to a centralized exchange."
+        image={ <DexImage /> }
+        text={[
+            "Our nodes expose the REST API allowing access to all blockchain data (transaction history, balances, etc.) as well as operations for creating a new transaction — it’s easy to make a transfer, exchange, issue of tokens or lease fund transactions programmatically.",
+            "The matcher exposes its own REST API for accessing the order book and submitting orders. This allows users to implement any kind of trading UI or trading robots."
+        ]}
+        buttons={[
+            (
+                <IconButton
+                    href="https://github.com/wavesplatform/Waves/wiki/Matcher"
+                    target="_blank"
+                    iconName="github"
+                >
+                    DEX API
                 </IconButton>
-            </Col>
-            <Col xs={12} md={6} lg={6}>
-                <img src={dexImageSrc} alt="DEX image" className={classes.image} />
-            </Col>
-        </Row>
-    </div>
+            )
+        ]}
+    />
 );
 
 
-export default injectSheet(styles)(FriendlyAPI);
+export default DEX;
---
+++ src/common/containers/Developers/lib/PictureBlock/index.jsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Typography from 'src/common/components/Typography';
+import Margin from 'src/common/components/Margin';
+import { Row, Col } from 'src/common/components/Grid';
+
+
+import injectSheet from 'react-jss';
+import cn from 'classnames';
+
+
+
+const styles = theme => ({
+    image: {
+        maxWidth: '100%'
+    },
+    buttonWrapper: {
+        marginBottom: theme.spacing.unit * 2,
+    },
+
+    'button-wrapper-left': {
+        composes: '$buttonWrapper',
+    },
+    'button-wrapper-right': {
+        composes: '$buttonWrapper',
+    },
+
+    [theme.mixins.atMedia('md')]: {
+        rowRight: {
+            flexDirection: 'row-reverse'
+        },
+        'button-wrapper-left': {
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            marginRight: theme.spacing.unit * 2,
+        },
+        'button-wrapper-right': {
+            display: 'inline-block',
+            verticalAlign: 'middle',
+            marginLeft: theme.spacing.unit * 2,
+        },
+
+        'text-left': {
+            paddingLeft: theme.spacing.unit * 4,
+            paddingRight: '10%'
+        },
+        'text-right': {
+            paddingRight: theme.spacing.unit * 4,
+            paddingLeft: '10%',
+            textAlign: 'right',
+        },
+
+        mdAlignCenter: {
+            textAlign: 'center'
+        }
+    }
+})
+
+
+const PictureBlock = ({
+    classes,
+    title,
+    subtitle,
+    image,
+    text,
+    buttons,
+    align,
+}) => (
+    <div>
+        <Row className={align === 'right' ? classes.rowRight : ''}>
+            <Col xs={12}>
+                <Row centered> 
+                    <Col xs={12} md={8} lg={7}>
+                        <Typography type="display2" className={classes.mdAlignCenter}>
+                            { title }
+                        </Typography>
+                        <Typography type="body2" className={classes.mdAlignCenter}>
+                            { subtitle }
+                        </Typography>
+                        <Margin bottom={4} />
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col xs={12} sm={8} md={6} lg={6}>
+                { image }
+            </Col>
+
+            <Col xs={12} md={6} lg={6} className={classes[`text-${align}`]}>
+                { typeof text === 'string' ? (
+                    <Typography type="body">
+                        { text }
+                    </Typography>
+                ) : (
+                    text.map((p, i) => <Typography type="body" key={`paragraph_${i}`}>{p}</Typography>)
+                ) }
+
+                <Margin bottom={2} />
+
+                { buttons.map((button, index) => (
+                    <div className={classes[`button-wrapper-${align}`]} key={`devs_block_button_${index}`}>
+                        { button }
+                    </div>
+                )) }
+            </Col>
+        </Row>
+    </div>
+);
+
+
+PictureBlock.defaultProps = {
+    align: 'left'
+}
+
+PictureBlock.propTypes = {
+    text: PropTypes.oneOfType(PropTypes.string, PropTypes.arrayOf(PropTypes.string))
+}
+
+
+export default injectSheet(styles)(PictureBlock);
