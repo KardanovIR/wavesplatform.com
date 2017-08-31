b67cb1969b54d92d68ca30b80ab4d1519f7e81b7
Dmitry Shuranov <dvshur@gmail.com>
2017-08-31 17:05:51 +0300

    fixed font-face to enable woff fonts

--- src/common/theme/typeface.js
+++ src/common/theme/typeface.js
@@ -2,36 +2,36 @@ const typeface = {
     '@font-face': [
         {
             fontFamily: 'TTNorms',
-            src: `url(${require('./fonts/TTNorms-Bold.woff2')})`,
-            fallbacks: [
-                { src: `url(${require('./fonts/TTNorms-Bold.woff')}) format(woff)` }
+            src: [
+                `url(${require('./fonts/TTNorms-Bold.woff2')})`,
+                `url(${require('./fonts/TTNorms-Bold.woff')}) format(woff)`
             ],
             fontWeight: 700,
             fontStyle: 'normal'
         },
         {
             fontFamily: 'TTNorms',
-            src: `url(${require('./fonts/TTNorms-Medium.woff2')})`,
-            fallbacks: [
-                { src: `url(${require('./fonts/TTNorms-Medium.woff')}) format(woff)` }
+            src: [
+                `url(${require('./fonts/TTNorms-Medium.woff2')})`,
+                `url(${require('./fonts/TTNorms-Medium.woff')}) format(woff)`
             ],
             fontWeight: 500,
             fontStyle: 'normal'
         },
         {
             fontFamily: 'TTNorms',
-            src: `url(${require('./fonts/TTNorms-Italic.woff2')})`,
-            fallbacks: [
-                { src: `url(${require('./fonts/TTNorms-Italic.woff')}) format(woff)` }
+            src: [
+                `url(${require('./fonts/TTNorms-Italic.woff2')})`,
+                `url(${require('./fonts/TTNorms-Italic.woff')}) format(woff)`
             ],
             fontWeight: 'normal',
             fontStyle: 'italic'
         },
         {
             fontFamily: 'TTNorms',
-            src: `url(${require('./fonts/TTNorms-Regular.woff2')})`,
-            fallbacks: [
-                { src: `url(${require('./fonts/TTNorms-Regular.woff')}) format(woff)` }
+            src: [
+                `url(${require('./fonts/TTNorms-Regular.woff2')})`,
+                `url(${require('./fonts/TTNorms-Regular.woff')}) format(woff)`
             ],
             fontWeight: 'normal',
             fontStyle: 'normal'
