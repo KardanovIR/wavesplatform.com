3f58500732fe26e7321dfda06642839520078836
Dmitry Shuranov <dvshur@gmail.com>
2017-09-01 19:03:50 +0300

    temporary local API test

--- src/server/middleware/getDexData.js
+++ src/server/middleware/getDexData.js
@@ -1,7 +1,8 @@
 import fetchJson from 'src/server/utils/fetchJson';
 
 const getDexData = async (ctx, next) => {
-    await fetchJson('http://13.56.20.226/general')
+    // await fetchJson('http://13.56.20.226/general')
+    await fetchJson('http://0.0.0.0:3000/general')
         .then(res => ctx.state.initialState = { ...ctx.state.initialState, dexData: res } )
         .catch(err => ctx.throw(500, 'Unable to fetch DEX general data'));
     await next();
