578609b552673ea2adea5b5f62924c6b4e6ee50f
Dmitry Shuranov <dvshur@gmail.com>
2017-09-04 11:45:32 +0300

    returned prod IP request address

--- src/server/middleware/getDexTopPairs.js
+++ src/server/middleware/getDexTopPairs.js
@@ -1,7 +1,7 @@
 import fetchJson from 'src/server/utils/fetchJson';
 
 const getDexTopPairs = async (ctx, next) => {
-    await fetchJson('http://0.0.0.0:3000/pairs')
+    await fetchJson('http://13.56.20.226/pairs')
         .then(res => ctx.state.initialState = { ...ctx.state.initialState, dexTopPairs: res } )
         .catch(err => ctx.throw(500, 'Unable to get DEX top pairs'));
 
