fe428fb3f3472fbe3a5a5815252745f8d6d0b7cb
Dmitry Shuranov <dvshur@gmail.com>
2017-10-09 16:32:22 +0300

    iframe size updated

--- src/common/containers/WavesNG/lib/StressTest/index.jsx
+++ src/common/containers/WavesNG/lib/StressTest/index.jsx
@@ -21,60 +21,83 @@ import Countdown from './Countdown';
 // export default connect(s => s, {
 //     onGetClientClick: () => getClientClick({ page: 'get-waves', source: 'how_to_buy_waves' }),
 // })(View);
+import injectSheet from 'react-jss';
+const styles = ({ breakpoints }) => ({
+    sheetWrapper: {
+        height: 150,
+    },
+    [breakpoints.up('md')]: {
+        sheetWrapper: {
+            height: 180,
+        },
+    },
+    [breakpoints.up('lg')]: {
+        sheetWrapper: {
+            height: 200,
+        },
+    }
+})
 
-const StressTest = () => (
-	<Panel>
-		<Margin top={2} bottom={4}>
-			<Countdown />
-		</Margin>
-		
-		<Divider />
-		<Margin bottom={4} />
 
-		<Row direction="row-reverse">
-			<Col xs={12} sm={5} lg={4}>
-				<Article
-					title={
-						<FormattedMessage
-							id="wavesNG.stressTest.chart1.title"
-							defaultMessage="UtxPool size"
-						/>
-					}
-					text={
-						<FormattedMessage
-							id="wavesNG.stressTest.chart1.text"
-							defaultMessage="Numeber of transaction waiting in queue for confirmation. The more trasnsactions are processed by network within a minute, the faster it decreases."
-						/>
-					}
-				/>
-			</Col>
-			<Col xs={12} sm={7} lg={8}>
-					chart1
-			</Col>
-		</Row>
-		<Margin bottom={4} />
-		<Row direction="row-reverse">
-			<Col xs={12} sm={5} lg={4}>
-				<Article
-					title={
-						<FormattedMessage
-							id="wavesNG.stressTest.chart1.title"
-							defaultMessage="UtxPool size"
-						/>
-					}
-					text={
-						<FormattedMessage
-							id="wavesNG.stressTest.chart1.text"
-							defaultMessage="Numeber of transaction waiting in queue for confirmation. The more trasnsactions are processed by network within a minute, the faster it decreases."
-						/>
-					}
-				/>
-			</Col>
-			<Col xs={12} sm={7} lg={8}>
-					chart1
-			</Col>
-		</Row>
-	</Panel>
+
+const StressTest = ({ classes }) => (
+    <Panel>
+        <Margin top={2} bottom={4}>
+            <Countdown />
+        </Margin>
+
+        <Divider />
+        <Margin bottom={4} />
+
+        <Row direction="row-reverse">
+            <Col xs={12} sm={5} lg={4}>
+                <Article
+                    title={
+                        <FormattedMessage
+                            id="wavesNG.stressTest.chart1.title"
+                            defaultMessage="UtxPool size"
+                        />
+                    }
+                    text={
+                        <FormattedMessage
+                            id="wavesNG.stressTest.chart1.text"
+                            defaultMessage="Numeber of transaction waiting in queue for confirmation. The more trasnsactions are processed by network within a minute, the faster it decreases."
+                        />
+                    }
+                />
+            </Col>
+            <Col xs={12} sm={7} lg={8}>
+                <div className={classes.chartWrapper}>
+                    <iframe src="http://grafana.wavesnodes.com/dashboard-solo/db/stress-test?orgId=2&panelId=1&theme=light" width="100%" height="100%" frameBorder="0" />
+                </div>
+
+            </Col>
+        </Row>
+        <Margin bottom={4} />
+        <Row direction="row-reverse">
+            <Col xs={12} sm={5} lg={4}>
+                <Article
+                    title={
+                        <FormattedMessage
+                            id="wavesNG.stressTest.chart2.title"
+                            defaultMessage="Transaction througput"
+                        />
+                    }
+                    text={
+                        <FormattedMessage
+                            id="wavesNG.stressTest.chart2.text"
+                            defaultMessage="Number of transactions mined (confirmed) by the blockchain per minute."
+                        />
+                    }
+                />
+            </Col>
+            <Col xs={12} sm={7} lg={8}>
+                <div className={classes.chartWrapper}>
+                    <iframe src="http://grafana.wavesnodes.com/dashboard-solo/db/stress-test?orgId=2&panelId=2&theme=light" width="100%" height="100%" frameBorder="0" />
+                </div>
+            </Col>
+        </Row>
+    </Panel>
 )
 
-export default StressTest;
+export default injectSheet(styles)(StressTest);
