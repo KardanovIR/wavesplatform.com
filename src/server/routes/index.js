f6eb1c5e073cf9f7c1e7846e11eec8bae282ebd6
Dmitry Shuranov <dvshur@gmail.com>
2017-10-10 20:27:51 +0300

    HOTFIX subdomain redirect

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -12,6 +12,7 @@ import Error404 from './get/Error404';
 import Developers from 'src/common/containers/Developers';
 import Company from 'src/common/containers/Company';
 import GetWaves from 'src/common/containers/GetWaves';
+import WavesNG from 'src/common/containers/WavesNG';
 
 
 // nested routers
@@ -28,6 +29,7 @@ router
     .get('/developers', render({ script: 'developers', component: Developers, description: 'Build applications on the blockchain' }))
     .get('/company', render({ script: 'company', component: Company, description: 'We change people’s lives' }))
     .get('/get-waves', render({ script: 'getWaves', component: GetWaves, description: 'Get Waves'  }))
+    .get('/waves-ng', render({ script: 'wavesNG', component: WavesNG, description: 'Help us test Waves-NG'  }))
     .use('/api', api.routes())
     .use('/subdomains', subdomains.routes())
     .get('*', Error404)
