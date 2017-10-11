bfa251a7f94099b15ab5d66295c292ebb6e2eaea
Dmitry Shuranov <dvshur@gmail.com>
2017-10-11 15:02:06 +0300

    removed /waves-ng route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -29,7 +29,6 @@ router
     .get('/developers', render({ script: 'developers', component: Developers, description: 'Build applications on the blockchain' }))
     .get('/company', render({ script: 'company', component: Company, description: 'We change people’s lives' }))
     .get('/get-waves', render({ script: 'getWaves', component: GetWaves, description: 'Get Waves'  }))
-    .get('/waves-ng', render({ script: 'wavesNG', component: WavesNG, description: 'Help us test Waves-NG'  }))
     .use('/api', api.routes())
     .use('/subdomains', subdomains.routes())
     .get('*', Error404)
