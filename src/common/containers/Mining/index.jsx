bac40f825b2e054b9b04024ea4c473880b3a616a
Dmitry Shuranov <dvshur@gmail.com>
2017-10-26 13:17:15 +0300

    Revenue template

--- src/common/containers/Mining/index.jsx
+++ src/common/containers/Mining/index.jsx
@@ -4,25 +4,28 @@ import React from 'react';
 import ThemeProvider from 'src/common/components/ThemeProvider';
 import Layout from 'src/common/components/Layout';
 import Section from 'src/common/components/Section';
-// import Background from 'src/common/components/Background';
+import Background from 'src/common/components/Background';
 import MainScreenBackground from 'src/common/components/Background/MainScreen';
 
 // import AnchorScroll from 'src/public/components/AnchorScroll';
-
-
-
 
 // lib
 import MainScreen from './lib/MainScreen';
 import Algorithms from './lib/Algorithms';
 import StartMining from './lib/StartMining';
+import Revenue from './lib/Revenue';
 
 import { FormattedMessage } from 'react-intl';
 
+import injectSheet from 'react-jss';
+const styles = theme => ({
+    bgRevenue: { 
+        background: '#f0f4fb'
+    },
+});
 
-const PageLayout = () => (
+const PageLayout = ({ classes }) => (
     <Layout>
-
         <MainScreenBackground type="dark">
             <MainScreen
                 title={
@@ -43,25 +46,25 @@ const PageLayout = () => (
         <Section top={3} bottom={4}>
             <Algorithms />
         </Section>
+
+        <Background className={classes.bgRevenue} skewed>
+            <Section size={4}>
+                <Revenue />
+            </Section>
+        </Background>
+
         <Section size={4}>
             <StartMining />
         </Section>
-
     </Layout>
 );
 
-
-
-const Page = PageLayout;
-
-
+const Page = injectSheet(styles)(PageLayout);
 
 const App = () => (
     <ThemeProvider>
         <Page />
     </ThemeProvider>
-)
-
-
+);
 
 export default App;
