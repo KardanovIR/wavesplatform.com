bf843451fb39cdaf3c175e326250c34a649b7815
Dmitry Shuranov <dvshur@gmail.com>
2017-10-30 18:16:56 +0300

    socket changed to prod value

--- src/common/containers/StressTest/lib/socket.js
+++ src/common/containers/StressTest/lib/socket.js
@@ -2,8 +2,8 @@ import io from 'socket.io-client';
 import { Observable } from 'rxjs';
 
 
-// const SOCKET_PATH = 'wss://stresstest.wavesplatform.com';
-const SOCKET_PATH = 'localhost:3002';
+const SOCKET_PATH = 'wss://stresstest.wavesplatform.com';
+// const SOCKET_PATH = 'localhost:3002';
 
 
 
