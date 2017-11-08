94d5a43c347c3adb5a854629b243340c489f2c7a
Dmitry Shuranov <dvshur@gmail.com>
2017-11-08 14:36:10 +0300

    added snackbar stress test

--- src/common/components/Layout/index.jsx
+++ src/common/components/Layout/index.jsx
@@ -3,47 +3,46 @@ import React from 'react';
 import Wrapper from '../Wrapper';
 import Navigation from '../Navigation';
 import Footer from '../Footer';
+import SnackbarStressTest from 'src/common/components/SnackbarStressTest';
 
 import links from './links';
 
 import injectSheet from 'react-jss';
 
-
 const styles = theme => ({
     root: {
-        overflowX: 'hidden'
+        overflowX: 'hidden',
     },
     navigationWrapper: {
-        marginBottom: theme.spacing.unit * 5
+        marginBottom: theme.spacing.unit * 5,
     },
     [theme.mixins.atMedia('md')]: {
         navigationWrapper: {
-            marginBottom: theme.spacing.unit * 10
+            marginBottom: theme.spacing.unit * 10,
         },
     },
     [theme.mixins.atMedia('lg')]: {
         navigationWrapper: {
-            marginBottom: theme.spacing.unit * 15
+            marginBottom: theme.spacing.unit * 15,
         },
-    }
-})
-
-
+    },
+});
 
 const Layout = ({
     children,
     classes,
     activeLink,
-    hideFooter
+    hideFooter,
+    hideSnackbar,
 }) => (
     <Wrapper>
         <div className={classes.navigationWrapper}>
             <Navigation links={links} activeLink={activeLink} />
         </div>
         <div>{children}</div>
-        { !hideFooter && <Footer links={links} /> }
+        {!hideFooter && <Footer links={links} />}
+        {!hideSnackbar && <SnackbarStressTest />}
     </Wrapper>
-)
-
+);
 
 export default injectSheet(styles)(Layout);
---
+++ src/common/components/SnackbarStressTest/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import View from './View';
+import { withLocalStorage } from 'src/public/hoc/localStorage';
+
+const EnhancedSnackbar = withLocalStorage(
+    'snackbarStressTest'
+)(({ onLocalStorageUpdate, initialValue, ...rest }) => (
+    <View
+        onClick={() => onLocalStorageUpdate('shown')}
+        onClose={() => onLocalStorageUpdate('shown')}
+		show={initialValue !== 'shown'}
+    />
+));
+
+export default EnhancedSnackbar;
