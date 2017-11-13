dfca8ad85a2034323d1120a31c6b6f713c12d313
Dmitry Shuranov <dvshur@gmail.com>
2017-11-13 12:44:37 +0300

    added reducer to /company SSR

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,6 +8,7 @@ import product from './get/product';
 import mining from './get/mining';
 import leasing from './get/leasing';
 import Error404 from './get/Error404';
+import company from './get/company';
 
 
 // page components
@@ -28,7 +29,7 @@ router
     .get('/', home)
     .get('/product', product)
     .get('/developers', render({ script: 'developers', component: Developers, description: 'Build applications on the blockchain' }))
-    .get('/company', render({ script: 'company', component: Company, description: 'We change people’s lives' }))
+    .get('/company', company)
     .get('/get-waves', render({ script: 'getWaves', component: GetWaves, description: 'Get Waves'  }))
     .get('/mining', mining)
     .get('/leasing', leasing)
