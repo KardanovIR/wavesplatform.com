1801bc1ac4cc5cec91f2ec5524675afd351fe1f3
SK <sk@ony.ru>
2017-12-13 18:04:10 +0300

    links: filter wip pages

--- src/common/components/Layout/links.js
+++ src/common/components/Layout/links.js
@@ -15,11 +15,11 @@ export const DESKTOP_LINKS = [
     page: 'developers',
     text: <FormattedMessage id="nav.developers" defaultMessage="Developers" />,
   },
-  {
-    href: url('company'),
-    page: 'company',
-    text: <FormattedMessage id="nav.company" defaultMessage="Company" />,
-  },
+  // {
+  //   href: url('company'),
+  //   page: 'company',
+  //   text: <FormattedMessage id="nav.company" defaultMessage="Company" />,
+  // },
   {
     href: url('get-waves'),
     text: <FormattedMessage id="nav.getWaves" defaultMessage="Get Waves" />,
@@ -43,24 +43,24 @@ export const MOBILE_LINKS = [
     text: <FormattedMessage id="nav.developers" defaultMessage="Developers" />,
     type: 'primary'
   },
-  {
-    href: url('company'),
-    page: 'company',
-    text: <FormattedMessage id="nav.company" defaultMessage="Company" />,
-    type: 'primary'
-  },
-  {
-    href: url('leasing'),
-    page: 'leasing',
-    text: <FormattedMessage id="nav.leasing" defaultMessage="Leasing" />,
-    type: 'secondary'
-  },
-  {
-    href: url('mining'),
-    page: 'mining',
-    text: <FormattedMessage id="nav.mining" defaultMessage="Mining" />,
-    type: 'secondary'
-  },
+  // {
+  //   href: url('company'),
+  //   page: 'company',
+  //   text: <FormattedMessage id="nav.company" defaultMessage="Company" />,
+  //   type: 'primary'
+  // },
+  // {
+  //   href: url('leasing'),
+  //   page: 'leasing',
+  //   text: <FormattedMessage id="nav.leasing" defaultMessage="Leasing" />,
+  //   type: 'secondary'
+  // },
+  // {
+  //   href: url('mining'),
+  //   page: 'mining',
+  //   text: <FormattedMessage id="nav.mining" defaultMessage="Mining" />,
+  //   type: 'secondary'
+  // },
   {
     href: url('files.terms_conditions'),
     text: <FormattedMessage id="nav.terms" defaultMessage="Terms and conditions" />,
