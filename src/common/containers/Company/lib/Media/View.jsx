a428663ffb713d6191a0f2bf299dd177e96e86fe
SK <sk@ony.ru>
2017-12-13 18:32:31 +0300

    Company tweaks

--- src/common/containers/Company/lib/Media/View.jsx
+++ src/common/containers/Company/lib/Media/View.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 import Typography from 'src/common/components/Typography';
 import Margin from 'src/common/components/Margin';
-import Button from 'src/common/components/Button';
-import Link from 'src/common/components/Link';
 import { Row, Col } from 'src/common/components/Grid';
-import Background from 'src/common/components/Background';
 
 import Panel from 'src/common/components/Panel';
 
@@ -14,39 +11,30 @@ import { FormattedMessage } from 'react-intl';
 import injectSheet from 'react-jss';
 import styles from './styles';
 
-
 import media from './media.json';
 
-
-
-const Media = ({
-    classes,
-}) => (
-        <div className={classes.root}>
-            <Row>
-                <Col xs={12} style={{padding: 0}}>
-                    <Typography type="display2" align="left" style={{fontSize: 23}}>
-                        <FormattedMessage
-                            id="company.media.title"
-                            defaultMessage="Media"
-                        />
-                    </Typography>
-                    <Margin bottom={1} />
-                </Col>
-            </Row>
-            <Row>
-                {media.map((contact, index) => (
-                    <Col key={`media_org_${index}`} style={{padding: 0}} xs={4} sm={4} md={4}>
-                        <a href={contact.url} target="_blank" className={classes.link}>
-                            <Panel className={classes.media}>
-                                <img src={contact.logo} className={classes.logo} alt="Media contact" />
-                            </Panel>
-                        </a>
-                    </Col>
-                ))}
-            </Row>
-        </div>
-    );
-
+const Media = ({ classes }) => (
+  <div className={classes.root}>
+    <Row>
+      <Col xs={12} style={{ padding: 0 }}>
+        <Typography type="display2" align="left" style={{ fontSize: 23 }}>
+          <FormattedMessage id="company.media.title" defaultMessage="Media" />
+        </Typography>
+        <Margin bottom={1} />
+      </Col>
+    </Row>
+    <Row>
+      {media.map((contact, index) => (
+        <Col key={`media_org_${index}`} style={{ padding: 0 }} xs={4} sm={4} md={4}>
+          <a href={contact.url} target="_blank" className={classes.link}>
+            <Panel className={classes.media}>
+              <img src={contact.logo} className={classes.logo} alt="Media contact" />
+            </Panel>
+          </a>
+        </Col>
+      ))}
+    </Row>
+  </div>
+);
 
 export default injectSheet(styles)(Media);
