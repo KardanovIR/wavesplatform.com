2ea88b9b47e8dfdc31360942807ca1169bf9eb8a
roman <nytuh2000@gmail.com>
2017-12-13 17:16:12 +0300

    some fixes

--- src/common/containers/Company/lib/Team/styles.js
+++ src/common/containers/Company/lib/Team/styles.js
@@ -1,17 +1,22 @@
 const styles = theme => ({
-    [theme.breakpoints.up('xs')]: {
-        Team: {
-            flexDirection: "row",
-            overflowY: "hidden",
-            flexWrap: 'nowrap',
+    Team: {
+        flexDirection: "row",
+        overflowY: "hidden",
+        flexWrap: 'nowrap',
 
-            '&::-webkit-scrollbar': {
-                width: 0,
-            },
-            '-ms-overflow-style': "none",
-            overflow: "-moz-scrollbars-none",
-        }
-    }
+        '&::-webkit-scrollbar': {
+            width: 0,
+        },
+        '-ms-overflow-style': "none",
+        overflow: "-moz-scrollbars-none",
+    },
+    [theme.breakpoints.up('md')]: {
+        Team: {
+            overflowY: "hidden",
+            flexWrap: 'wrap',
+        },
+    },
+
 })
 
 export default styles;
