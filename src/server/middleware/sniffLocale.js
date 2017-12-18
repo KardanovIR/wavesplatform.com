c109865d9544fecad1b370c99a5af2f16ea2dd16
Dmitry Shuranov <dvshur@gmail.com>
2017-12-18 16:24:49 +0300

    updated sniffLocale to use cookies

--- src/server/middleware/sniffLocale.js
+++ src/server/middleware/sniffLocale.js
@@ -1,20 +1,26 @@
 import { Locales } from 'locale';
 
-const supported = new Locales(['ru', 'en']);
+const supported = ['ru', 'en'];
 
 const sniffLocale = async (ctx, next) => {
   const cookie = ctx.cookies.get('locale');
+
   if (cookie && ~supported.indexOf(cookie)) {
     ctx.locale = cookie;
   } else {
     const sniffedLocale = new Locales(ctx.req.headers['accept-language'], 'en')
-      .best(supported)
+      .best(new Locales(supported))
       .toString();
 
+    ctx.cookies.set('locale', sniffedLocale, {
+      maxAge: 15778476000, // 0.5 years
+      domain: '*.wavesplatform.com',
+    });
+
     ctx.locale = sniffedLocale;
-    ctx.locale = 'ru';
-    await next();
   }
+
+  await next();
 };
 
 export default sniffLocale;
