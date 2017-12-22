b6292a75e17e22c8b6e1c941ee8ca189634bdfa5
Dmitry Shuranov <dvshur@gmail.com>
2017-12-22 13:09:13 +0300

    real links to news

--- src/common/containers/WavesNG/lib/News/index.jsx
+++ src/common/containers/WavesNG/lib/News/index.jsx
@@ -15,7 +15,7 @@ const News = () => {
           <Link
             textDecoration={false}
             target="_blank"
-            href="https://wavescommunity.com"
+            href="https://blog.wavesplatform.com/waves-ng-stress-test-results-in-44090f59bb15"
             inverted
           >
             <FormattedMessage id="wavesNG.news.1" />
@@ -28,7 +28,7 @@ const News = () => {
           <Link
             textDecoration={false}
             target="_blank"
-            href="https://wavescommunity.com"
+            href="https://www.forbes.com/forbes/welcome/?toURL=https://www.forbes.com/sites/rogeraitken/2017/11/08/waves-set-to-become-fastest-decentralized-blockchain-platform-globally/&refURL=&referrer=#6a3f27d938b6"
             inverted
           >
             <FormattedMessage id="wavesNG.news.2" />
