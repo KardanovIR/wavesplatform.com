586dd069287c92d1f830f32308c15b01fbb84c84
Ivan Shmidt <bahoc47@gmail.com>
2018-01-26 17:28:43 +0300

    Removed animation due to ssr problems

--- src/common/components/MainScreen/styles/index.js
+++ src/common/components/MainScreen/styles/index.js
@@ -4,11 +4,6 @@ import flexCentered from 'src/common/styles/flexCentered';
 const styles = theme => ({
   ...image(theme),
 
-  '@keyframes show-0': {
-    from: { opacity: 0, transform: 'translateY(5px)' },
-    to: { opacity: 1, transform: 'translateY(0)' },
-  },
-
   xsHidden: {
     display: 'none',
   },
@@ -26,8 +21,7 @@ const styles = theme => ({
 
   title: {
     lineHeight: 1.05,
-    opacity: 0,
-    animation: 'show-0 .65s ease .5s forwards',
+    opacity: 1,
   },
 
   imageColMargin: {
@@ -39,15 +33,13 @@ const styles = theme => ({
     fontSize: 16,
     lineHeight: 2,
     color: ({ inverted }) => (inverted ? theme.palette.gray[0] : 'inherit'),
-    opacity: 0,
-    animation: 'show-0 .65s ease 1s forwards',
+    opacity: 1,
   },
 
   buttonsContainer: {
     margin: [0, -theme.spacing.unit * 0.5],
     display: 'flex',
-    opacity: 0,
-    animation: 'show-0 .65s ease 1s forwards',
+    opacity: 1,
     justifyContent: 'center',
   },
 
