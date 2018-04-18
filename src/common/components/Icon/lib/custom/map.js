a3f531c194bea694b7ad80d4f04e31b83d269235
Ivan Shmidt <bahoc47@gmail.com>
2018-04-18 13:14:33 +0300

    Added discord link and icon

--- src/common/components/Icon/lib/custom/map.js
+++ src/common/components/Icon/lib/custom/map.js
@@ -2,10 +2,12 @@ import Hamburger from './Hamburger';
 import Cross from './Cross';
 import GooglePlay from './GooglePlay';
 import MapMarker from './MapMarker';
+import DiscordIcon from '!svg-react-loader!./Discord.svg';
 
 export default {
   hamburger: Hamburger,
   cross: Cross,
   googlePlay: GooglePlay,
   mapMarker: MapMarker,
+  discord: DiscordIcon,
 };
