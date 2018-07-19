f08634a1b977bef8be384d0cb2c183321f4fbe9f
Ivan Shmidt <bahoc47@gmail.com>
2018-07-19 12:05:25 +0300

    Tuned positioning a bit

--- src/common/containers/Main/lib/WavesClient/styles.js
+++ src/common/containers/Main/lib/WavesClient/styles.js
@@ -9,11 +9,11 @@ const styles = theme => ({
   },
   imageCol: {
     position: 'absolute',
-    right: 0,
     width: '100%',
+    left: 0,
+    right: 0,
     bottom: -100,
     margin: [theme.spacing.unit * 4, 0, theme.spacing.unit * 2],
-    maxWidth: 420,
   },
   imageWrapper: {
     position: 'relative',
@@ -22,27 +22,26 @@ const styles = theme => ({
     overflow: 'hidden',
   },
   image: {
+    maxWidth: 420,
     display: 'block',
     zIndex: -1,
     top: 0,
-    left: 0,
     width: '100%',
+    margin: '0 auto',
   },
-  [theme.breakpoints.up('tablet')]: {
-    imageCol: {
-      right: '10vw',
-    },
-  },
+
   [theme.breakpoints.up('sm')]: {
     root: {
       marginBottom: 0,
     },
     imageCol: {
+      left: 'auto',
+      right: '10%',
       bottom: -110,
       width: '50%',
-      maxWidth: '100%',
     },
     image: {
+      maxWidth: 'none',
       width: 'auto',
     },
   },
