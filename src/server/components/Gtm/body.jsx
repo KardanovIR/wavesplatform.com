84c231b5193a31fc4c93c1859ad3971f5e4f0a3b
Dmitry Shuranov <dvshur@gmail.com>
2018-10-02 16:22:25 +0300

    stage environment gtm

--- src/server/components/Gtm/body.jsx
+++ src/server/components/Gtm/body.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GtmBody = () => (
   <noscript>
     <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-PQDMZMJ"
+      src="https://www.googletagmanager.com/ns.html?id=GTM-KR7HQLF&gtm_auth=Gopq5B1SAzYLShTywB5yHQ&gtm_preview=env-147&gtm_cookies_win=x"
       height="0"
       width="0"
       style={{ display: 'none', visibility: 'hidden' }}
