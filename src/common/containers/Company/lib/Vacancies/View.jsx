0ad30a613ebdb39469c802f8590c394c9a6dc4c3
vba2000 <vba@mail.ru>
2019-01-18 14:33:23 +0300

    WEB-286: fix styles

--- src/common/containers/Company/lib/Vacancies/View.jsx
+++ src/common/containers/Company/lib/Vacancies/View.jsx
@@ -2,36 +2,31 @@ import React from 'react';
 
 import Typography from 'src/common/components/Typography';
 import Margin from 'src/common/components/Margin';
-import { Row, Col } from 'src/common/components/Grid';
+import {Row, Col} from 'src/common/components/Grid';
 
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 import injectSheet from 'react-jss';
 import styles from './styles';
 import Button from 'src/common/components/Button';
 
-const VacanciesContainer = ({ classes, onVacanciesClick }) => (
-  <Row centered>
-    <Col xs={12} sm={10} md={10}>
-      <div className={classes.div}>
-        <Row>
-          <Col xs={12} sm={10} md={9}>
+const VacanciesContainer = ({classes, onVacanciesClick}) => (
+    <Row className={classes.div}>
+        <Col xs={12} md={8} lg={5}>
             <Typography type="display2" className={classes.text} align="left">
-              <FormattedMessage id="company.vacancies.text" />
+                <FormattedMessage id="company.vacancies.text"/>
             </Typography>
-          </Col>
-        </Row>
-        <Margin bottom={1} />
 
-        <Button
-            onClick={onVacanciesClick}
-            href='https://job.wavesplatform.com'
-            target="_blank"
-            secondary>
-          <FormattedMessage id="company.vacancies.title" />
-        </Button>
-      </div>
-    </Col>
-  </Row>
+            <Margin bottom={3}/>
+
+            <Button
+                onClick={onVacanciesClick}
+                href='https://job.wavesplatform.com'
+                target="_blank"
+                secondary>
+                <FormattedMessage id="company.vacancies.title"/>
+            </Button>
+        </Col>
+    </Row>
 );
 
 export default injectSheet(styles)(VacanciesContainer);
