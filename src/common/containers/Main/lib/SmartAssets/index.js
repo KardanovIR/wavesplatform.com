16596dce6c4586817ff1c4c5ce2c7df1f45b7ef0
vba2000 <vba@mail.ru>
2019-01-18 12:06:24 +0300

    WEB-289: remove code

--- src/common/containers/Main/lib/SmartAssets/index.js
+++ src/common/containers/Main/lib/SmartAssets/index.js
@@ -11,110 +11,21 @@ import styles from './styles';
 import injectSheet from "react-jss";
 import ContentContainer from 'src/common/components/ContentContainer';
 
-const TIME_REF = {
-    DAY: 1000 * 60 * 60 * 24,
-    HOUR: 1000 * 60 * 60,
-    MINUTE: 1000 * 60,
-    SECOND: 1000
-};
-
-const Digit = injectSheet(styles)(({time, text, isLast, classes}) => {
-    return <div className={classes.digest} style={{minHeight: '90px'}}>
-        <div style={{ display: 'inline-block' }}>
-            <div style={{ display: 'inline-block' }}>
-                <Typography type="display6" style={{ lineHeight: '0.6' }}>
-                    {time}
-                    {isLast ? null : <span className={classes.dots}>:</span>}
-                </Typography>
-            </div>
-            <div >
-                <Typography type="body">
-                    <FormattedPlural
-                        value={time}
-                        one={
-                            <FormattedMessage
-                                id={`home.smartAssets.${text}.one`}
-                            />
-                        }
-                        few={
-                            <FormattedMessage
-                                id={`home.smartAssets.${text}.few`}
-                            />
-                        }
-                        other={
-                            <FormattedMessage
-                                id={`home.smartAssets.${text}.other`}
-                            />
-                        }
-                    />
-                </Typography>
-            </div>
-        </div>
-    </div>
-});
 
 class SmartAssetsView extends React.PureComponent {
 
-    state = { showCounter: true,  timeDiff: null, days: 0, hours: 0, minutes: 0, seconds: 0, initData: false };
-
     constructor(props) {
         super(props);
-        this.getTime();
-    }
-
-    async getTime() {
-        const data = await fetch('https://nodes.wavesplatform.com/utils/time');
-        const { system } = await data.json();
-        const myTime = Date.now();
-        this.setState({ timeDiff: 1546829001000 - system, myTime  });
-    }
-
-    updateTime() {
-        clearTimeout(this.timer);
-        this.timer = setTimeout(() => this.updateTime(), 200);
-
-        if (this.state.timeDiff == null) {
-            return null;
-        }
-
-        const time = this.state.timeDiff - (Date.now() - this.state.myTime);
-
-        if (!time) {
-            return null;
-        }
-
-
-        if (time < 0) {
-            this.setState({ showCounter: false });
-            clearTimeout(this.timer);
-            return null;
-        }
-
-        const days = parseInt(time / TIME_REF.DAY);
-        const hours = parseInt((time - days * TIME_REF.DAY) / TIME_REF.HOUR);
-        const minutes = parseInt((time - days * TIME_REF.DAY - hours * TIME_REF.HOUR) / TIME_REF.MINUTE) || '';
-        const seconds = parseInt((time - days * TIME_REF.DAY - hours * TIME_REF.HOUR - minutes * TIME_REF.MINUTE ) / TIME_REF.SECOND) || '';
-
-        this.setState({ days, hours, minutes, seconds, initData: true });
-    }
-
-    componentWillMount() {
-         this.updateTime();
-    }
-
-    componentWillUnmount() {
-        clearTimeout(this.timer);
     }
 
     render() {
         const { classes } = this.props;
-        const { showCounter, days, hours, minutes, seconds, initData } = this.state;
 
         return (
             <div className={classes.bgAssets}>
                 <ContentContainer>
                     <Row>
-                        {showCounter ? null : <Col xs={11} sm={8}>
+                        <Col xs={11} sm={8}>
                             <Typography type="display3" className={classes.smartAssetTitle}>
                                 <FormattedMessage id="home.smartAssets.title.text"
                                                   defaultMessage="Smart Assets feature is activated on Waves MainNet!"
@@ -129,22 +40,7 @@ class SmartAssetsView extends React.PureComponent {
                                 />
                             </Button>
                             <Margin bottom={4}/>
-                        </Col>}
-                        {!showCounter || !initData ? null : <Col xs={12} sm={12}>
-                            <Typography type="display2" className={classes.smartAssetNotActiveTitle}>
-                                <FormattedMessage id='home.smartAssets.activated.title'
-                                                  defaultMessage="Smart Assets will be activated in"
-                                />
-                            </Typography>
-                            <Margin bottom={3}/>
-                            <div style={{minWidth: '350px'}}>
-                                <Digit className={classes.digest} time={days} text='days'/>
-                                <Digit className={classes.digest} time={hours} text='hours'/>
-                                <Digit className={classes.digest} time={minutes} text='minutes'/>
-                                <Digit className={classes.digest} time={seconds} text='seconds' isLast={true}/>
-                            </div>
-                            <Margin bottom={4}/>
-                        </Col>}
+                        </Col>
                     </Row>
                 </ContentContainer>
             </div>
